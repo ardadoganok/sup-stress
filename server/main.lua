ca02bfc4d124cce2f13846b91bfa5e0371f44c2d
Supreme <49347739+adoganok@users.noreply.github.com>
2020-09-17 15:00:53 +0300

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -6,13 +6,8 @@ RegisterServerEvent("sup-stress:add")
 AddEventHandler("sup-stress:add", function (value)
 	local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
-
-	-- if xPlayer.job.name == "police" then
-	-- 	TriggerClientEvent("esx_status:add", _source, "stress", value / 10)
-	-- else
+		
         TriggerClientEvent("esx_status:add", _source, "stress", value)
-        -- TriggerClientEvent('sup-notif', _source, 'Stresin arttı', 3)
-	-- end
 end)
 
 RegisterServerEvent("sup-stress:remove")
@@ -21,5 +16,5 @@ AddEventHandler("sup-stress:remove", function (value)
 	local xPlayer = ESX.GetPlayerFromId(_source)
 
     TriggerClientEvent("esx_status:remove", _source, "stress", value)
-    TriggerClientEvent('sup-notif', _source, 'Stresin azaldı', 3)
+    TriggerClientEvent('notification', _source, 'Stresin azaldı', 3)
 end)
